ab82569bb846596389589176bc69ca9c6e73f4d6
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-01 12:55:50 +0100

    implemented first version (#1)

---
+++ src/EventStore.PositionRepository/ConnectionBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EventStore.ClientAPI;
+
+namespace EventStore.PositionRepository
+{
+    public class ConnectionBuilder : IConnectionBuilder
+    {
+        public Uri ConnectionString { get; }
+        public ConnectionSettings ConnectionSettings { get; }
+        public string ConnectionName { get; }
+
+        public IEventStoreConnection Build()
+        {
+            return EventStoreConnection.Create(ConnectionSettings, ConnectionString, ConnectionName);
+        }
+
+        public ConnectionBuilder(Uri connectionString, ConnectionSettings connectionSettings, string connectionName)
+        {
+            ConnectionString = connectionString;
+            ConnectionSettings = connectionSettings;
+            ConnectionName = connectionName;
+        }
+    }
+}
