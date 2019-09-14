53698100d36beade9f644c90e0f9fb07f89ae870
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-14 13:37:28 +0100

    Implemented a custom IPositionRepositoryLogger to detach from any other logger; provided a SimpleConsoleLogger implementation

---
+++ src/EventStore.PositionRepository.TestClient/NLogPositionRepositoryLogger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NLog;
+
+namespace EventStore.PositionRepository.TestClient
+{
+    public class NLogPositionRepositoryLogger : IPositionRepositoryLogger
+    {
+        private readonly Logger _logger;
+
+        public NLogPositionRepositoryLogger(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Warn(string message, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string message, string warnMessage)
+        {
+            _logger.Warn(message, warnMessage);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, string errorMessage)
+        {
+            _logger.Error(message, errorMessage);
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+    }
+}
---
+++ src/EventStore.PositionRepository/IPositionRepositoryLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventStore.PositionRepository
+{
+    public interface IPositionRepositoryLogger
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Warn(string message, Exception ex);
+        void Warn(string message, string warnMessage);
+        void Error(string message);
+        void Error(string message, Exception ex);
+        void Error(string message, string errorMessage);
+        void Debug(string message);
+    }
+}
